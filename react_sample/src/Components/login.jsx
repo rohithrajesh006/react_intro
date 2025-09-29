d002ee38dd939af78fe51a9d1e748cd728305ca8
rohithrajesh006 <rohithrajesh006@gmail.com>
2025-09-29 13:47:21 +0530

    use state -data

--- react_sample/src/Components/login.jsx
+++ react_sample/src/Components/login.jsx
@@ -1,15 +1,30 @@
-
+import { useState } from "react";
 
 function Login(){
+    const [data,setData]=useState({
+        fullname:"",
+        email:"",
+        password:""
+    });
+     const handleChange=(e)=>{
+        const{name,value}=e.target;
+        
+        setData((prev)=>({...prev,[name]:value}))
+     }
+     const handleSubmit=(e)=>{
+        e.preventDefault();
+        console.log(data)
+     }
 
     return(
         <>
         <div>
             <h2>Login</h2>
-        <form action="">   
-          <input type="text" name="fullname" placeholder="Fullname" /><br />
-          <input type="password" name="password" placeholder="pasword" /><br />
-          <button>login</button>
+        <form onSubmit={handleSubmit}>   
+          <input type="text" name="fullname" placeholder="Fullname" value={data.fullname} onChange={handleChange} /><br />
+          <input type="password" name="password" placeholder="password"value={data.password} onChange={handleChange}  /><br />
+          <input type="email" name="email" placeholder="Email" value={data.email} onChange={handleChange} /><br />
+          <input type="submit" value="Login"/>
         </form>   
         </div>
         </>
